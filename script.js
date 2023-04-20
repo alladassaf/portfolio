66d1387f35d20fec2057719a4292e305ed989566
alladinassaf <50091700+alladinassaf@users.noreply.github.com>
2023-04-19 19:57:07 -0500

    updated portfolio

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
-const heroText = document.querySelector('.hero h1')
+const navTags = document.querySelectorAll('ul li')
+const sections = document.querySelectorAll('section')
+const indicator = document.querySelector('.indicator')
+const options = {
+    threshold: 0.5
+}
 
-let index = 0
+window.onscroll = () => {
+    if (pageYOffset >= 300) {
+        
+    }
+}
+
+
+const sectionObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            addShowClass(entry.target)
+            sections.forEach((sec, index) => {
+                if (index != 0) {
+                    const sectionChildren = [...sec.children]
+                    if (sections[sections.length - 1] == entry.target.parentElement) {
+                        sectionChildren.forEach(child => {
+                            sectionObserver.unobserve(child)
+                            console.log("unobserved")
+                        })
+                    }
+                }
+                
+            })
+        }
+    })
+}, options)
+
 
 function addShowClass(element) {
     element.classList.add("show")
 }
+
+sections.forEach((sec, index) => {
+    if (index != 0) {
+        const sectionChildren = [...sec.children]
+
+        sectionChildren.forEach(child => {
+            sectionObserver.observe(child)
+        })
+    }
+    
+})
